f858cf3d54dd1e988853c8e7087464f06120a880
MohamedTealeb <mohamedtealeb088@gmail.com>
2025-05-06 20:57:51 +0300

    Sidebar

---
+++ src/Modules/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Box, CircularProgress } from '@mui/material';
+import Sidebar from '../../Component/Shared/Sidebar';
+
+
+const drawerWidth = 240;
+
+export default function Home() {
+ 
+
+  return <>
+  <Sidebar/>
+  
+
+    
+
+
+
+  </>
+    
+}
